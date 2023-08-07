f583ebd7aea0dd56cb4191dcca9ea30dd1ca9dfc
Radhika Nagardona <radhika.nagardona@aspiresys.com>
2023-08-08 03:20:07 +0530

    handson

---
+++ handson/Day2/ArithmeticOperators1.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class ArithmeticOperators1 {
+
+	public static void main(String[] args) {
+		byte b1 = 12;
+		byte b2 = 13;
+		
+		byte b3 = (byte)(b1 + b2); 
+		System.out.println(b3);
+	}
+
+}
